b195173d2dca9df7f52af879d64a78ca3fbb77bd
kasty <kastiel_0991@mail.ru>
2024-04-27 21:14:53 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function start() {
 
   // Включаем CORS для HTTP запросов
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
